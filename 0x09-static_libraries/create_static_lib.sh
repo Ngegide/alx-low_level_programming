b7438d980f875fb82a645f8d8be82804767f1aaf
Ngegide <123301090+Ngegide@users.noreply.github.com>
2023-09-02 18:14:37 +0000

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 gcc -c *.c
-ar rc liball.a *-0
+ar rcs libmy.a
+ar rc libmy.a *o
+gcc -wall -pedantic -werror -wextra -c *.c
+ar -rc liball.a *.o
 ranlib liball.a
+ar -t libmy.a
+nm libmy.a
